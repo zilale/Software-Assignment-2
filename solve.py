cf07d929a68e3fd98d3004471090909889ef68c2
Žilvinas <z.aleksa@student.tue.nl>
2023-10-16 13:14:00 +0200

    force push

---
+++ solve.py
@@ -0,0 +1,60 @@
+##
+# 2WF90 Algebra for Security -- Software Assignment 2
+# Polynomial and Finite Field Arithmetic
+# solve.py
+#
+#
+# Group number:
+# group_number 
+#
+# Author names and student IDs:
+# Žilvinas aleksa 1703749
+# Ainius Sinickas 1704919
+# Jonas van der Schaaf 1700101
+# Gustas Šimkus 1704917
+##
+
+# Import built-in json library for handling input/output 
+import json
+
+
+
+def solve_exercise(exercise_location : str, answer_location : str):
+    """
+    solves an exercise specified in the file located at exercise_location and
+    writes the answer to a file at answer_location. Note: the file at
+    answer_location might not exist yet and, hence, might still need to be created.
+    """
+    
+    # Open file at exercise_location for reading.
+    with open(exercise_location, "r") as exercise_file:
+        # Deserialize JSON exercise data present in exercise_file to corresponding Python exercise data 
+        exercise = json.load(exercise_file)
+        
+
+    ### Parse and solve ###
+
+    # Check type of exercise
+    if exercise["type"] == "polynomial_arithmetic":
+        # Check what task within the polynomial arithmetic tasks we need to perform
+        if exercise["task"] == "addition":
+            # Solve polynomial arithmetic addition exercise
+            pass
+        elif exercise["task"] == "subtraction":
+            # Solve polynomial arithmetic subtraction exercise
+            pass
+        # et cetera
+    else: # exercise["type"] == "finite_field_arithmetic"
+        # Check what task within the finite field arithmetic tasks we need to perform
+        if exercise["task"] == "addition":
+            # Solve finite field arithmetic addition exercise
+            pass
+        # et cetera
+
+
+    # Open file at answer_location for writing, creating the file if it does not exist yet
+    # (and overwriting it if it does already exist).
+    with open(answer_location, "w") as answer_file:
+        # Serialize Python answer data (stored in answer) to JSON answer data and write it to answer_file
+        json.dump(answer, answer_file, indent=4)
+    
