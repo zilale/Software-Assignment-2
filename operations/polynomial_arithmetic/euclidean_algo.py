70c5e542dd44e68962f02ab88c1536b555e06f18
Žilvinas <z.aleksa@student.tue.nl>
2023-10-27 23:15:13 +0200

    feat:

--- operations/polynomial_arithmetic/euclidean_algo.py
+++ operations/polynomial_arithmetic/euclidean_algo.py
@@ -1,8 +1,13 @@
 
+from operations.polynomial_arithmetic.polynomial_addition import polynomial_addition
 from operations.polynomial_arithmetic.polynomial_multiplication import polynomial_multiplication
 from operations.polynomial_arithmetic.polynomial_subtraction import polynomial_subtraction
-from operations.polynomial_arithmetic.polynomial_longdivision import multiply_constant, polynomial_long_division, reduce_coefficients, remove_leading_zeros
+from operations.polynomial_arithmetic.polynomial_longdivision import polynomial_long_division, remove_leading_zeros
 
+# Function to answeriply two polynomials f and g under the given modulus m
+# Input: Two polynomials f and g in F[x] represented as arrays of coeffihelper1ents
+#        and a modulus m
+# Output: A polynomial representing f * g under the given modulus m
 def reduce_polynomials(a, b, p):
     h = a.copy()
     for i in range(0, len(h)):
@@ -14,51 +19,57 @@ def reduce_polynomials(a, b, p):
 
     return remove_leading_zeros(h), remove_leading_zeros(h2)
 
-def extended_euclidean_algorithm(f, g, p):
-    a, b = reduce_coefficients(f, p), reduce_coefficients(g, p)
-    switch = False
-    switch = len(a) < len(b) or (len(a) == len(b) and a[::-1] < b[::-1])
-    if switch:
-        a, b = b, a
+# Input: Two polynomials f and g in F[x] represented as arrays of coefficients
+#        and a modulus m
+# Output: A polynomial representing f + g under the given modulus m
+def extended_euclidean_algorithm(a, b, m):
+        x, v, y, u = [1], [1], [0], [0]
+        flag = False
 
-    x = [1]
-    v = [1]
-    y = [0]
-    u = [0]
+        for i in range(len(b)):
+            if b[i] != 0:
+                flag = True
 
-    # if f is 0 or g is 0, return None
-    while b != [0]:
-        q, r = polynomial_long_division(a, b, p)
-        a = b.copy()
-        b = r.copy()
-        x_p = x.copy()
-        y_p = y.copy()
-        x = u.copy()
-        y = v.copy()
-        u = polynomial_subtraction(x_p, polynomial_multiplication(q, u, p), p)
-        v = polynomial_subtraction(y_p, polynomial_multiplication(q, v, p), p)
-    
-    lc_a = a[len(a) - 1]
-    lc_a_inversed = pow(lc_a, -1, p)
+        while flag == True:
 
+            q,r = polynomial_long_division(a, b, m)
 
-    x_final = multiply_constant(x, lc_a_inversed, p)
-    y_final = multiply_constant(y, lc_a_inversed, p)
-    gcd = multiply_constant(a, lc_a_inversed, p)
-    
+            a = b
+            b = r
 
-    if switch:
-        x_final, y_final = y_final, x_final
+            x_p = x
+            y_p = y
+            x = u
+            y = v
+            
+            u = polynomial_subtraction(x_p, polynomial_multiplication(q, u, m), m)
+            v = polynomial_subtraction(y_p, polynomial_multiplication(q, v, m), m)
+            flag = False
 
-    return x_final, y_final, gcd
+            for i in range(len(b)):
+                if b[i] != 0:
+                    flag = True
 
-def test_extended_euclidean_algorithm():
-    # Test case 3s
-    f = [0, 0, 1, 1]
-    g = [1, 1, 1]
-    p = 2
-    expected_a = [1, 1]
-    expected_b = [1, 1, 1]
-    expected_gcd = [1]
-    assert extended_euclidean_algorithm(f, g, p) == (expected_a, expected_b, expected_gcd)
+        least_c_f = a[len(a) - 1]
+        lc_f_inversed = [pow(least_c_f, -1, m)]
+        
+        x_ans = polynomial_multiplication(x, lc_f_inversed, m)
+        y_ans = polynomial_multiplication(y, lc_f_inversed, m)
+
+        remove_leading_zeros(x_ans)
+        remove_leading_zeros(y_ans)
+
+        gcd = polynomial_addition(polynomial_multiplication(a,x_ans,m),polynomial_multiplication(b,y_ans,m),m)
+
+        return x_ans, y_ans, remove_leading_zeros(gcd)
+
+# def test_extended_euclidean_algorithm():
+#     # Test case 3s
+#     f = [0, 0, 1, 1]
+#     g = [1, 1, 1]
+#     p = 2
+#     expected_a = [1, 1]
+#     expected_b = [1, 1, 1]
+#     expected_gcd = [1]
+#     assert extended_euclidean_algorithm(f, g, p) == (expected_a, expected_b, expected_gcd)
 # test_extended_euclidean_algorithm()
--- operations/polynomial_arithmetic/polynomial_longdivision.py
+++ operations/polynomial_arithmetic/polynomial_longdivision.py
@@ -1,47 +1,36 @@
+from operations.polynomial_arithmetic.polynomial_addition import polynomial_addition
+from operations.polynomial_arithmetic.polynomial_multiplication import polynomial_multiplication
 from operations.polynomial_arithmetic.polynomial_subtraction import polynomial_subtraction
 
-def multiply_constant(f, c, p):
-    h = f.copy()
-    for i in range(0, len(h)):
-        h[i] = (c*h[i]) % p
-    return h
-
-def reduce_coefficients(f, p):
-    h = f.copy()
-    for i in range (0, len(h)):
-        h[i] = (h[i]) % p
-        
-    return remove_leading_zeros(h)
-
-def remove_leading_zeros(a):
-    c = 0
-    revA = reversed(a)
-    for item in revA:
-        if item != 0:
-            if c > 0:   return a[:-c]
-            else:       return a
-        c += 1
+def remove_leading_zeros(f):
+    help = 0
+    reversed = reversed(f)
+    for i in reversed:
+        if i != 0:
+            if help > 0:   return f[:-help]
+            else:       return f
+        help += 1
     return [0]
-    
-def multiply_n(f: list, n: int):
-    return [0]*n + f
 
-def polynomial_long_division(m: list, n: list, p: int):
-    f = remove_leading_zeros(reduce_coefficients(m, p))
-    g = remove_leading_zeros(reduce_coefficients(n, p))
-    if len(g) == 0 or g == [0]: # Division by zero or an empty polynomial
-        return None
-    if len(f) < len(g) or ((len(f) == 1 and len(g) == 1) and f[0] < g[0]): 
-        #f is of a smaller degree than g or they are both constants and f is smaller than g
-        return ([0], f)
-    q = [0] * len(f) # A maximum size list for the quotient of all zeros
-    r = f.copy()
-    while (len(r) >= len(g) and r != [0]):
-        #scalar =  lc(r) * (lc(g))^(-1)
-        scalar = (r[len(r) - 1] * pow(g[len(g) - 1], -1, p)) % p
-        degree_difference = len(r) - len(g) 
-        # r = r - X^(degree_difference) * g * scalar
-        r = polynomial_subtraction(r, multiply_n(multiply_constant(g, scalar, p), degree_difference),p)
-        q[degree_difference] = scalar # q = q + scalar*X^(degree_difference)
-    return (remove_leading_zeros(q), remove_leading_zeros(r))
+#Function to divide two polynomials f and g under the given modulus m
+def polynomial_long_division(a, b, m):
+    q = [0]
+    r = a
+    while ((len(r) - 1) >= (len(b) - 1)):
+        intermediate = []
+        for i in range(len(r) - len(b) + 1):
+            intermediate.append(0)
+        intermediate[len(r) - len(b)] = r[len(r) - 1] * pow(b[len(b) - 1], -1, m)
+        q = polynomial_addition(q, intermediate, m)
+        secondIntermediate = []
+
+        for i in range(len(r) - len(b) + 1):
+            secondIntermediate.append(0)
+        secondIntermediate[len(r) - len(b)] = (r[len(r) - 1] * pow(b[len(b) - 1], -1, m))
+        r = polynomial_subtraction(r, polynomial_multiplication(secondIntermediate, b, m), m)
+        #removing any leading zeroes
+        del r[len(r) - 1]
+    q[-1] = q[-1]%m
+
+    return remove_leading_zeros(q), remove_leading_zeros(r)
 
